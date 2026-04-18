f8e8feb3ac3e9ffa9ea253da37fe1acf806ab651
Kobakichi <kobakichi@KobakichinoAir.lan>
2026-04-18 23:11:45 +0900

    (#78) tauriの開発環境のセットアップ

---
+++ Sereal/src-tauri/src/serial/mod.rs
@@ -0,0 +1,6 @@
+pub mod controller;
+pub mod service;
+pub mod types;
+pub mod utils;
+
+pub use types::BaudRate;
---
+++ Sereal/src-tauri/src/serial/types.rs
@@ -0,0 +1,34 @@
+use std::fmt;
+
+#[derive(Default, Debug, PartialEq, Clone, Copy)]
+pub enum BaudRate {
+    BaudRate9600 = 9600,
+    #[default]
+    BaudRate115200 = 115200,
+}
+
+impl BaudRate {
+    pub fn iter() -> impl Iterator<Item = BaudRate> {
+        [BaudRate::BaudRate9600, BaudRate::BaudRate115200]
+            .iter()
+            .copied()
+    }
+}
+
+impl fmt::Display for BaudRate {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let rate = match self {
+            BaudRate::BaudRate9600 => 9600,
+            BaudRate::BaudRate115200 => 115200,
+        };
+        write!(f, "{}", rate)
+    }
+}
+
+#[derive(Default, Clone)]
+pub struct ReceivedData {
+    pub id: u64,
+    pub text: String,
+}
+
+pub const MAX_RECEIVED_DATA_SIZE: usize = 2048;
